95bdd1837161d942d5b6905078436276faa298b4
MXCALLL <idbellam12@gmail.com>
2025-07-08 12:53:36 +0100

    refactor: change the way I init struct

--- src/init_structs.c
+++ src/init_structs.c
@@ -3,27 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   init_structs.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: muidbell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: muidbell <muidbell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 20:36:58 by muidbell          #+#    #+#             */
-/*   Updated: 2025/06/14 20:09:20 by muidbell         ###   ########.fr       */
+/*   Updated: 2025/07/08 12:41:05 by muidbell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-t_philos *initialize_structs(int ac, char **av)
+long get_current_time(void)
 {
-	t_table		*table;
-	t_philos	*philo;
+	struct timeval start;
+
+	if (gettimeofday(&start, NULL))
+		ft_putstr("get time failed");
+
+	return (start.tv_sec * 1000 + start.tv_usec / 1000);
+}
+
+t_table	*init_table(t_table *table, int ac, char **av)
+{
 	size_t		i;
 
-	table = malloc(sizeof(t_table));
-
-	table = (t_table *)malloc(sizeof(t_table *));
+	table = ft_calloc(1, sizeof(t_table));
 	if(!table)
-		return(ft_putstr("Malloc fail"), NULL);
-
+		return(ft_putstr("Malloc fails"), NULL);
 	table->meals_per_philo = 0;
 	table->someone_died = 0;
 	table->num_philos = ft_atoi(av[1]);
@@ -35,13 +40,48 @@ t_philos *initialize_structs(int ac, char **av)
 	if (!table->num_philos || !table->time_to_die
 		|| !table->time_to_eat || !table->time_to_sleep)
 			return (free(table), NULL);
-	table->forks = malloc(sizeof(pthread_mutex_t) * table->num_philos);
+	table->forks = ft_calloc(table->num_philos, sizeof(pthread_mutex_t));
 	if (!table->forks)
 		return (free(table), ft_putstr("malloc forks fail"), NULL);
-
+	i = 0;
 	while(i < table->num_philos)
-		pthread_mutex_init(&table->forks[i], NULL);
+		pthread_mutex_init(&table->forks[i++], NULL);
 	pthread_mutex_init(&table->print_mutex, NULL);
+	table->start_time_ms = get_current_time();
+	return (table);
+}
 
-	// table->start_time_ms = gettimeofday();
+t_philos	*init_philos(t_table *table)
+{
+	t_philos	*philo;
+	size_t		i;
+	int			status;
+
+	philo = ft_calloc(table->num_philos, sizeof(t_philos));
+	if (!philo)
+		return(ft_putstr("Malloc fails"), NULL);
+
+	i = 0;
+	while (i < table->num_philos)
+	{
+		philo[i].id = i + 1;
+		philo[i].meals_eaten = 0;
+		philo[i].last_meal_timing = get_current_time();
+		philo[i].left_fork = &table->forks[i];
+		philo[i].right_fork = &table->forks[(i + 1) % table->num_philos];
+		philo[i].table = table;
+		status = pthread_create(&philo[i].thread, NULL, philo_simulation, &philo[i]);
+		if (status != 0)
+			return (ft_putstr("failed creating threads"), NULL);
+		i++;
+	}
+
+	i = 0;
+	while (i < table->num_philos)
+	{
+		pthread_join(philo[i].thread, NULL);
+		i++;
+	}
+
+	return (philo);
 }
