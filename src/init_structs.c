43dc06ee7a72dd5dffa45ee7bc1f17ffccaac305
MXCALLL <idbellam12@gmail.com>
2025-07-14 16:05:48 +0100

    fix thread leak

--- src/init_structs.c
+++ src/init_structs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_structs.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: muidbell <muidbell@student.42.fr>          +#+  +:+       +#+        */
+/*   By: muidbell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 20:36:58 by muidbell          #+#    #+#             */
-/*   Updated: 2025/07/13 20:24:50 by muidbell         ###   ########.fr       */
+/*   Updated: 2025/07/14 16:04:58 by muidbell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,22 @@ t_table	*init_table(t_table *table, int ac, char **av)
 	return (table);
 }
 
+void thread_leak_prevention(t_philos *philo, t_table *table, size_t failed_index)
+{
+	size_t	i;
+
+	pthread_mutex_lock(&table->death_mutex);
+	table->someone_died = 1;
+	pthread_mutex_unlock(&table->death_mutex);
+
+	i = 0;
+	while (i < failed_index)
+	{
+		pthread_join(philo[i].thread, NULL);
+		i++;
+	}
+}
+
 int	create_threads(t_philos *philo, t_table *table)
 {
 	pthread_t	monitor;
@@ -79,7 +95,10 @@ int	create_threads(t_philos *philo, t_table *table)
 		status = pthread_create(&philo[i].thread, NULL, philo_simulation,
 				&philo[i]);
 		if (status != 0)
+		{
+			thread_leak_prevention(philo, table, i);
 			return (ft_putstr("failed creating threads"), free(philo), 1);
+		}
 		i++;
 	}
 	status = pthread_create(&monitor, NULL, monitor_routine, table);
